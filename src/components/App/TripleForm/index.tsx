08452d7167be03a5e44814dc922aa18a8da7e12d
Sebastian Stoff <SebastianStoff@gmx.at>
2020-02-14 18:46:25 +0100

    reading out data and generating the input fields

--- src/components/App/TripleForm/index.tsx
+++ src/components/App/TripleForm/index.tsx
@@ -4,19 +4,69 @@ import ResponsiveForm from "../../common/ResponsiveForm"
 
 interface SearchParameter {
   label: string,
-  RESTParameter: string,
+  value: string,
+  id: string;
+  placeHolder?: string;
+  RESTParameter?: string
 }
 
 interface props {
-  parameterDelimiter?: string
-  parameters?: SearchParameter[]
+  queryStart?: string;
+  parameterDelimiter?: string;
+  parameters?: SearchParameter[];
+  encode?: boolean;
 }
 
-const TripleForm: React.FC<props> = ({parameterDelimiter, parameters}) => {
+const TripleForm: React.FC<props> = ({queryStart, parameterDelimiter = ";", parameters = [{
+  label: "test01",
+  value:"",
+  id:"123",
+  placeHolder:"hahatest",
+  RESTParameter: "$1|"
+},
+{
+  label:"test02",
+  value:"bla",
+  id:"123",
+  placeHolder:"bimbi",
+  RESTParameter: "$2|"
+},
+{
+  label:"test03",
+  value:"blasdas",
+  id:"1233333",
+  placeHolder:"bimbiasdds",
+  RESTParameter: "$3|"
+},
+
+]}) => {
+
+  const [query, setQuery] = React.useState<""| string>("");
+  const [inputs, setInputs] = React.useState<SearchParameter[]>(parameters);
+
+  React.useEffect(()=>{
+    if(!inputs) return;
+    console.log("Inputs changed!", inputs);
+
+    let query = "";
+    query += queryStart;
+    inputs.forEach(input => {
+      if(query==="undefined")query = "";
+      query += `${input.RESTParameter}${input.value}${parameterDelimiter}`;
+    });
+    
+    if(query==="undefined")return setQuery("");
+    setQuery(query)
+  }, [inputs]);
+
   return (
+    <>
+    <p>{query}</p>
     <ResponsiveForm
-      inputFields={[{label:"test", id:"123", placeHolder:"test-placeholder"}]}
+      inputFields={inputs}
+      setInputFields={setInputs}
     ></ResponsiveForm>
+    </>
   );
 };
 
