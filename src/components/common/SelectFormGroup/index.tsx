ed691ee78d7964160cdd6158956a973adc3a93b9
Sebi <SebastianStoff@gmx.at>
2020-02-17 16:19:07 +0100

    also able to get value now

--- src/components/common/SelectFormGroup/index.tsx
+++ src/components/common/SelectFormGroup/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 interface props {
     options: {value: string, label: string}[],
+    handleGetSelection?: (selection: string) => void;
 }
 
-const SelectFormGroup:React.FC<props> = ({options}) => {
+const SelectFormGroup:React.FC<props> = ({options, handleGetSelection}) => {
 
     const [selectedVal, setSelectedVal] = React.useState<string>(options[0].value);
 
     const handleSelection = (evt: React.ChangeEvent<HTMLSelectElement>) => {
             setSelectedVal(evt.currentTarget.value);
+            if(handleGetSelection)handleGetSelection(selectedVal);
     }
 
   return (
