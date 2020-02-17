36b4a4cdd94c8a80d5f71f59ecec6ca12a60266b
Sebi <SebastianStoff@gmx.at>
2020-02-17 19:10:13 +0100

    passing onchange down to component

--- src/components/common/SelectFormGroup/index.tsx
+++ src/components/common/SelectFormGroup/index.tsx
@@ -3,15 +3,17 @@ import React from "react";
 interface props {
     options: {value: string, label: string}[],
     handleGetSelection?: (selection: string) => void;
+    onChange?: (evt:any) => void
 }
 
-const SelectFormGroup:React.FC<props> = ({options, handleGetSelection}) => {
+const SelectFormGroup:React.FC<props> = ({options, handleGetSelection, onChange}) => {
 
     const [selectedVal, setSelectedVal] = React.useState<string>(options[0].value);
 
     const handleSelection = (evt: React.ChangeEvent<HTMLSelectElement>) => {
             setSelectedVal(evt.currentTarget.value);
             if(handleGetSelection)handleGetSelection(selectedVal);
+            if(onChange)onChange(evt);
     }
 
   return (
@@ -19,7 +21,7 @@ const SelectFormGroup:React.FC<props> = ({options, handleGetSelection}) => {
         <label>Example multiple select - Selected value = {selectedVal}</label>
       <select className="form-control" value={selectedVal} onChange={(evt)=>handleSelection(evt)}>
         {options.map((option, index) => (
-            <option key={`SelectFormGroup_option_${index}`} value={option.value}>{ option.label }</option>
+            <option key={`SelectFormGroup_option_${index}`} value={option.value} data-index={index}>{ option.label }</option>
         ))}
       </select>
     </div>
