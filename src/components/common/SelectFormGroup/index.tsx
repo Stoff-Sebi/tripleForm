215e5f3bd38fdd427309f3ee9085e822fbe13eda
Sebi <SebastianStoff@gmx.at>
2020-02-17 16:15:15 +0100

    added impl of state of selection

--- src/components/common/SelectFormGroup/index.tsx
+++ src/components/common/SelectFormGroup/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 interface props {
-    options: {value: string, label: string}[]
+    options: {value: string, label: string}[],
 }
 
 const SelectFormGroup:React.FC<props> = ({options}) => {
+
+    const [selectedVal, setSelectedVal] = React.useState<string>(options[0].value);
+
+    const handleSelection = (evt: React.ChangeEvent<HTMLSelectElement>) => {
+            setSelectedVal(evt.currentTarget.value);
+    }
+
   return (
     <div className="form-group">
-      <label>Example multiple select</label>
-      <select className="form-control">
+        <label>Example multiple select - Selected value = {selectedVal}</label>
+      <select className="form-control" value={selectedVal} onChange={(evt)=>handleSelection(evt)}>
         {options.map((option, index) => (
             <option key={`SelectFormGroup_option_${index}`} value={option.value}>{ option.label }</option>
         ))}
