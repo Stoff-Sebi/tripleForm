75c20d6430bec3ac47eb3df3dae151fda7902c69
Sebi <SebastianStoff@gmx.at>
2020-02-27 15:31:54 +0100

    using LocalStorageApplier. closes #26

--- src/components/common/StorageInput/index.tsx
+++ src/components/common/StorageInput/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LocalStorageApplier from "../LocalStorageApplier";
 
 interface props {
   onChange: (value: string) => void;
@@ -22,34 +23,19 @@ const StorageInput: React.FC<props> = ({
 }) => {
   const [inputValue, setInputValue] = React.useState<string>(value);
 
-  React.useEffect(() => {
-    if (localStorageKey === undefined) return;
-    if (inputValue === undefined) return;
-
-    //read out from local storage
-    let storageValue = localStorage.getItem(localStorageKey);
-    storageValue = storageValue !== null ? storageValue : inputValue;
-    setInputValue(storageValue);
-  }, []);
-
-  React.useEffect(() => {
-    onChange(inputValue);
-    if (!localStorageKey) return;
-    if (inputValue === undefined || inputValue === null)
-      localStorage.setItem(localStorageKey, "");
-
-    localStorage.setItem(localStorageKey, inputValue ? inputValue : "");
-  }, [inputValue, localStorageKey]);
-
   return (
     <>
-      <p>{localStorageKey}</p>
-      <p>{useLocaleStorage}</p>
-      <input
-        onChange={evt => setInputValue(evt.currentTarget.value)}
+      <LocalStorageApplier
+        storageKey={localStorageKey}
         value={inputValue}
-        {...properties}
-      ></input>
+        getLoadedValue={(val)=>setInputValue(val)}
+      >
+        <input
+          onChange={evt => setInputValue(evt.currentTarget.value)}
+          value={inputValue}
+          {...properties}
+        ></input>
+      </LocalStorageApplier>
     </>
   );
 };
