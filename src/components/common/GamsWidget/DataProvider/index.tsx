74af1cd7a2ec5d7217a0b5128ef19964d1f9f58e
Sebi <SebastianStoff@gmx.at>
2020-08-04 08:48:26 +0200

    Added GamsWidget component

---
+++ src/components/common/GamsWidget/DataProvider/index.tsx
@@ -0,0 +1,170 @@
+import React from "react";
+import { WidgetDataProviderProps, GamsWidgetDataSource, GamsWidgetType } from "..";
+import { getCurrentPid, copyDeep } from "../../../../utils/gamsUtils";
+
+
+const DataProvider: React.FC<WidgetDataProviderProps> = ({ widgetDef, setWidgetData }) => {
+
+  React.useEffect(() => {
+    widgetDef.lifecycle = widgetDef.lifecycle ? widgetDef.lifecycle : "develop"; 
+    let lifecycle = widgetDef.lifecycle;
+
+    // when no dataSpec -> assuming none necessary.
+    //@ts-ignore
+    if(widgetDef.dataSourcesSpec){
+      //fetch data according to dataSpec
+      if(lifecycle === "develop") console.debug("GamsWidget-DataProvider: dataSourceSpecification provided for the widget: ", widgetDef);
+
+      //async IIFE to await + load + merge of data when multiples
+      (async () => {
+        // simultaneously fetch data - and then await merge?
+        // own method that can be awaited
+        
+        //@ts-ignore
+        //let reqCount: number = widgetDef.dataSpec.length;
+        //let reqCompletedCounter: number = 0;
+        
+        //@ts-ignore (is ok although dataSpec must be defined)
+        widgetDef.dataSourcesSpec.sources.forEach(source => {
+          
+
+
+
+
+        });
+
+
+      })();
+
+
+      //TODO -> loop over all data specs. (not only the first)
+      //TODO -> currently only supports json!
+      //atm take only first data spec into account
+      let dataSource = widgetDef.dataSourcesSpec.sources[0];
+      if(!checkDataSource(dataSource))return console.error("GamsWidget-DataProvider: Validation of data source object failed. Aborting operations: ", dataSource);
+      
+      
+      //when dataSource is a digital object.
+      //following code clause build the correct "request url"
+      if(dataSource.gamsDigitalObj){
+
+        // build "first part" of url first according to pid definition.
+        let pid = dataSource.gamsDigitalObj.pid ? dataSource.gamsDigitalObj.pid : getCurrentPid();
+        let reqUrlStart = `${window.location.origin}/archive/objects/${pid}`;
+        let reqUrlFull = '';
+
+        // both defined doesn't make any sense.
+        if(dataSource.gamsDigitalObj.datastream && dataSource.gamsDigitalObj.service)return console.error("GamsWidget-DataProvider: Got a dataSource object where both a datastream and a service is defined. Please make sure to define either one: ", dataSource);
+
+        //first case of datastreams (same in every case)
+        if(dataSource.gamsDigitalObj.datastream){
+          reqUrlFull = reqUrlStart + `/datastreams/${dataSource.gamsDigitalObj.datastream}/content`;
+
+          // case of service should be called.
+        } else {
+          
+          // call standard service or specified one specific for content model
+          switch(dataSource.gamsDigitalObj.contentModel){
+            case "GML":
+              reqUrlFull = reqUrlStart + `/methods/sdef:GML/${dataSource.gamsDigitalObj.service ? dataSource.gamsDigitalObj.service : "getJSON"}`;
+              break;
+            case "TEI":
+              reqUrlFull = reqUrlStart + `/methods/sdef:TEI/${dataSource.gamsDigitalObj.service ? dataSource.gamsDigitalObj.service : "get"}`;
+              break;
+            case "Context":
+              reqUrlFull = reqUrlStart + `/methods/sdef:Context/${dataSource.gamsDigitalObj.service ? dataSource.gamsDigitalObj.service : "get"}`;
+              break;
+            case "Query":
+              reqUrlFull = reqUrlStart + `/methods/sdef:Query/${dataSource.gamsDigitalObj.service ? dataSource.gamsDigitalObj.service : "getJSON"}`;
+              break;
+            default:
+              return console.error("GamsWidget-DataProvider: Unsupported-ContentModel-Error at building the request url for specified content model: ", dataSource.gamsDigitalObj.contentModel, dataSource);
+          }
+
+        }
+
+        // when request url correctly built for gamsObject -> start fetch.
+        if(lifecycle === "develop") console.debug("GamsWidget-DataProvider: Sending request for widget data to the dataspecification to: ", reqUrlFull);
+        fetch(reqUrlFull).then(resp => {
+          resp.json().then(json => {
+            let widgetDefCopy: GamsWidgetType = copyDeep(widgetDef);
+            widgetDefCopy.data = json;
+            if(lifecycle === "develop") console.debug("GamsWidget-DataProvider: Succesfully got data - setting now data property of widget definition. Got data: ", json);
+            setWidgetData(widgetDefCopy);
+          }).catch(err => {
+            console.error("GamsWidget-DataProvider: Error parsing data as JSON from datasource: ", dataSource);
+            console.error(err);
+          });
+          
+        }).catch(err => {
+            console.error("GamsWidget-DataProvider: Error getting data from url: ", reqUrlFull);
+            console.error(err);
+        });
+
+      } 
+
+
+
+    } else {
+      if(lifecycle === "develop") console.debug("GamsWidget-DataProvider: No data provided for the widget - Assuming not needed for widget definition: ", widgetDef);
+      setWidgetData(copyDeep(widgetDef));
+    }
+  }, [ widgetDef, setWidgetData ]);
+
+  return null
+
+}
+
+export default DataProvider;
+
+
+
+
+/**
+ * Checks if exactly one property of given GamsWidgetDataSource object is defined.
+ *@param dataSource GamsWidgetDataSource object to be validated. 
+ *@returns True if exactly one property is defined. False if none or multiple are defined.
+ */
+const checkDataSource = (dataSource: GamsWidgetDataSource): boolean => {
+  // adds all properties validated to true to the array.
+  let definedProps = Object.values(dataSource).filter(val => val ? val : false);
+  if(definedProps.length === 1){
+    console.info("GamsWidget-DataProvider: Validation of Datasource successfull: ", dataSource);
+    return true;
+  } else if(definedProps.length === 0){
+    console.error("GamsWidget-DataProvider: All properties of given DataSource validated false. Please make sure to assign one correct property to the given datasource object: ", dataSource);
+    return false;
+  } else {
+    console.error("GamsWidget-DataProvider: More than one property is defined in given datasource. Make sure to define only one: ", dataSource);
+    return false;
+  }  
+}
+
+
+
+export const harvestFeatures = (
+  urls: string[],
+): Object => {
+  if (urls.length === 0)
+    throw new RangeError(
+      "Only arrays greater than length 0 are valid for the harvestPersons function."
+    );
+
+  // array of Promises
+  // fetch and parse as json
+  let respsPromises = urls.map(url => fetch("url").then(resp => resp.json())); 
+
+  // resolve all promises
+  // coming from 
+  Promise.all(respsPromises).then(values => {
+
+    //merge array of json objects to one -> difficult if datatype is not known!
+    //best to merge on feature level?
+
+
+  }).catch(err => console.error(err));
+
+
+  return respsPromises
+  
+};
---
+++ src/components/common/GamsWidget/WidgetDefProvider/index.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { getCurrentPid, getProjectAbbr } from "../../../../utils/gamsUtils";
+import { GamsWidgetType } from "../index"
+import { WidgetDefProviderProps } from ".."
+
+// Extends WidgetDefProviderProps so that additional 
+// props can be passed down (on level of <GamsWidget/> call)
+// can only be optional properties.
+interface GAMSWidgetDefProps extends WidgetDefProviderProps {
+  globalPropName?: string;
+  datastream?: string;
+  objectPidRef?: "context";   // if set to undefined -> assuming current object? via datastream?
+                           // widget's that are configured by one specific object?
+                           // like a context object.  
+}
+
+/**
+ * Component checks if and where the global config object was assigned and
+ * sets state of parent component. (for the configuration / basal widget definition).
+ * globalProp overwrites datastream -> objectPidRef overwrites default behavior of reqeuesting client's
+ * current object's pid.
+ * @param globalPropName Name of the property of the global window object where the widget definition could be held.
+ * @param datastream Name of datastream where the widget definition could be.
+ * @param objectPidRef Pid of object from where the widget definition should be requested.
+ */
+const WidgetDefProvider: React.FC<GAMSWidgetDefProps> = ({
+  setDefinition = undefined, 
+  children = undefined,
+  globalPropName,
+  datastream,     
+  //maybe best to set object reference to "managed content" | "reference" | "x" ? like in cirillo?
+  // or contextObject with datastream?
+  // or service to call
+  objectPidRef
+}) => {
+  React.useEffect(() => {
+    if (!setDefinition) return;
+
+    // configured window object validates true
+    if(window[globalPropName as any]){
+      return setDefinition(window[globalPropName as any]);
+    } 
+    
+    // window object not defined
+    // try to fetch for GUI def in datastream
+    // first assign PID.
+
+    // when ne objectPidRef is set to context assign
+    // the context pid
+    let reqPid = (objectPidRef === "context") ? `context:${getProjectAbbr()}` : getCurrentPid();
+
+    if(datastream){ 
+      let requestUrl = `${window.location.origin}/archive/objects/${reqPid}/datastreams/${datastream}/content`  
+      // fetch object's widget datastream
+      fetch(requestUrl).then(data => {
+        data.text().then(text => {
+          try {
+            console.debug("GamsWidget-WidgetDefinitionProvider: No window-object definition provided. Defaulted to requesting current object's datastream at url: ", requestUrl);
+            setDefinition(JSON.parse(text));
+          } catch(e){
+            //parse xml to JSON via lib best?
+          }
+        }).catch(err => {
+          console.error("GamsWidget- WidgetDefProvider: Error stringifying the datastream at: ", requestUrl);
+          console.error(err);
+        });
+      }).catch(err => {
+        console.error("GamsWidget- WidgetDefProvider: Error at getting widget definition from url: ", requestUrl);
+        console.error(err);
+      })
+    } else {
+      console.error("GamsWidget- WidgetDefProvider: No global window property nor datastream defined for retrieving the widget's definition. Returning dummy-definition instead. (But the app might crash).");
+      let dummyDef: GamsWidgetType  = {
+        name:"Dummy-Test Widget",
+        lifecycle:"develop"        
+      }
+      console.error("GamsWidget- WidgetDefProvider: Set dummy definition: ", dummyDef);
+      setDefinition(dummyDef);    
+    }
+
+  }, [setDefinition, globalPropName, datastream, objectPidRef]);
+
+  return children ? children : null;
+};
+
+export default WidgetDefProvider;
