25ef659ee1c5647cb8b3ecb8e0d9a30a6fa42a8c
Sebi <SebastianStoff@gmx.at>
2020-09-06 19:20:54 +0200

    #37. Updated triple form root container

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TripleFormWidget from './components/App';
 
-ReactDOM.render(<TripleFormWidget />, document.getElementById('root'));
+ReactDOM.render(<TripleFormWidget />, document.getElementById('GAMS_WIDGET_TRIPLEFORM'));
