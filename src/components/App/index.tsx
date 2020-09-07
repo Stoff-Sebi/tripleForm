480af2e55c9fade4a996c1d94c8fd229819aef77
Sebi <SebastianStoff@gmx.at>
2020-09-07 10:25:19 +0200

    updated component names

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,8 +5,8 @@ import TripleForm from "./TripleForm";
 const TripleFormWidget: React.FC = () => {
   return (
     <GamsWidget
-      datastream="WIDGET_TRIPLE_FORM"
-      globalPropName="__gamsWidget__TripleForm"
+      datastream="GAMS_WIDGET_TRIPLEFORM"
+      globalPropName="GAMS_WIDGET_TRIPLEFORM"
     >
       <TripleForm/>
     </GamsWidget>
